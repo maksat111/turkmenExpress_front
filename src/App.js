fea2fab0d5d426a249d9ca9cde83893ce2943bb0
maksat111 <maksatakmyradow111@gmail.com>
2023-02-08 04:56:59 +0500

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Logo from './images/logo.png';
+import Home from './pages/home/Home';
+
+const Navbar = lazy(() => import('./components/Navbar'));
+const Footer = lazy(() => import('./components/Footer'));
 
 function App() {
   return (
-    <div>
-      Hello
-    </div>
+    <Suspense fallback={<img src={Logo} alt='Loading...' />}>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+      <Footer />
+    </Suspense>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,66 @@
 import React from 'react';
+import { AiOutlineSearch, AiOutlineClockCircle, AiOutlineGlobal, AiOutlineCustomerService, AiOutlineHeart, AiOutlineUser, AiOutlineShoppingCart } from 'react-icons/ai'
+import { Badge } from 'antd';
+import './Navbar.css';
+import Logo from '../images/logo.png';
 
 function Navbar() {
+
+    const iconStyle = { color: "red", fontSize: "26px" }
+
+    const items = [
+        {
+            icon: <AiOutlineClockCircle style={iconStyle} />,
+            text: '09:00-21:00',
+            border: true
+        },
+        {
+            icon: <AiOutlineGlobal style={iconStyle} />,
+            text: 'TM',
+            border: true
+        },
+        {
+            icon: <AiOutlineCustomerService style={iconStyle} />,
+            text: 'Habarlaşmak',
+            border: true
+        },
+        {
+            icon: <AiOutlineHeart style={iconStyle} />,
+            text: 'Halananlar',
+            border: true
+        },
+        {
+            icon: <AiOutlineUser style={iconStyle} />,
+            text: 'Profil',
+            border: false,
+        }
+    ]
+
     return (
         <div className='navbar-container'>
-            Footer
+            <div className='navbar-header'>
+                <div className='navbar-logo-container'>
+                    <img src={Logo} alt='logo' />
+                </div>
+                <div className='navbar-search-container'>
+                    <input placeholder='Gozleg' />
+                    <AiOutlineSearch className='search' />
+                </div>
+                <div className='navbar-items-container'>
+                    {items.map(item =>
+                        <div className='navbar-item' style={!item.border ? {} : { borderRight: '1px rgb(180, 180, 180) solid' }} key={item.text}>
+                            {item.icon}
+                            <p>{item.text}</p>
+                        </div>
+                    )}
+                </div>
+                <div className='navbar-sebet'>
+                    <Badge count='5' >
+                        <AiOutlineShoppingCart style={iconStyle} />
+                    </Badge>
+                    <p>156.40 manat</p>
+                </div>
+            </div>
         </div>
     );
 }
