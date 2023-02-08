e75cc0aadcf5dd2b541aace3b52a3ec6bc9348f4
maksat111 <maksatakmyradow111@gmail.com>
2023-02-08 12:51:40 +0500

    change

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { lazy, Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Logo from './images/logo.png';
-import Home from './pages/home/Home';
 
 const Navbar = lazy(() => import('./components/Navbar'));
 const Footer = lazy(() => import('./components/Footer'));
+const Category = lazy(() => import('./components/Category'));
+const Home = lazy(() => import('./pages/home/Home'));
 
 function App() {
   return (
     <Suspense fallback={<img src={Logo} alt='Loading...' />}>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/home' element={< Category />} >
+          <Route path='/home/category' element={<Home />} />
+        </Route>
       </Routes>
       <Footer />
     </Suspense>
